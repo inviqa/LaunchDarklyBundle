03745281ee4d597cd5e3b4ab9c06a1fcc7fb895d
Alex Davyskiba <sanya.davyskiba@gmail.com>
2023-03-10 22:05:26 +0200

    Fix tests.

--- features/bootstrap/app/TestController.php
+++ features/bootstrap/app/TestController.php
@@ -5,25 +5,31 @@ namespace Inviqa\LaunchDarklyBundle\Tests;
 use Inviqa\LaunchDarklyBundle\Client\StaticClient;
 use Inviqa\LaunchDarklyBundle\Client\ExplicitUser\StaticClient as UserStaticClient;
 use LaunchDarkly\LDUser;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Psr\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
-class TestController extends AbstractController
+class TestController implements ContainerAwareInterface
 {
+    use ContainerAwareTrait;
+
     private $client;
     private $userClient;
     private $testService;
     private $aliasedTestService;
     private $taggedTestService;
+    private Environment $twig;
 
     public function __construct(
+        Environment $twig,
         $client,
         $userClient,
         $testService,
         $aliasedTestService,
         $taggedTestService
     ) {
+        $this->twig = $twig;
         $this->client = $client;
         $this->userClient = $userClient;
         $this->testService = $testService;
@@ -31,50 +37,44 @@ class TestController extends AbstractController
         $this->taggedTestService = $taggedTestService;
     }
 
-    public function setContainer(ContainerInterface $container): ?ContainerInterface
-    {
-        $this->container = $container;
-        return $container;
-    }
-
-    public function indexAction() {
+    public function indexAction(): \Symfony\Component\HttpFoundation\Response {
         if ($this->client->variation('new-homepage-content')) {
             return new Response("<html><body>the new homepage content</body></html>");
         }
         return new Response("<html><body>the old homepage content</body></html>");
     }
 
-    public function indexUserAction() {
+    public function indexUserAction(): \Symfony\Component\HttpFoundation\Response {
         if ($this->userClient->variation('new-homepage-user-content', new LDUser('user-id'))) {
             return new Response("<html><body>the new homepage user content</body></html>");
         }
         return new Response("<html><body>the old homepage user content</body></html>");
     }
 
-    public function templatedAction() {
-        return $this->render('index.html.twig');
+    public function templatedAction(): \Symfony\Component\HttpFoundation\Response {
+        return new Response($this->twig->render('index.html.twig'));
     }
 
-    public function serviceAction() {
+    public function serviceAction(): \Symfony\Component\HttpFoundation\Response {
         return new Response($this->testService->getContent());
     }
 
-    public function aliasedServiceAction() {
+    public function aliasedServiceAction(): \Symfony\Component\HttpFoundation\Response {
         return new Response($this->aliasedTestService->getContent());
     }
 
-    public function taggedServiceAction() {
+    public function taggedServiceAction(): \Symfony\Component\HttpFoundation\Response {
         return new Response($this->taggedTestService->getContent());
     }
 
-    public function staticAction() {
+    public function staticAction(): \Symfony\Component\HttpFoundation\Response {
         if (StaticClient::variation('new-static-access-content')) {
             return new Response("<html><body>the new static access content</body></html>");
         }
         return new Response("<html><body>the old static access content</body></html>");
     }
 
-    public function staticUserAction() {
+    public function staticUserAction(): \Symfony\Component\HttpFoundation\Response {
         if (UserStaticClient::variation('new-static-access-user-content', new LDUser('user-id'))) {
             return new Response("<html><body>the new static access user content</body></html>");
         }
